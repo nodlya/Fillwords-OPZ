d0d66599952e2da57c87cb29e6fd8514576b1f07
nodlya <nadya-popova-02@mail.ru>
2021-06-01 01:33:42 +0500

    2-3.0 Added library (unsuccessfully) + refactored (too successfully)

---
+++ RPN/RPNLibrary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strForPolish = Console.ReadLine();
+            var converter = new Converter(strForPolish);
+            Console.WriteLine("Обратная польская запись " 
+                              + converter.ResultStringRPN + " равно "
+                              + Convert.ToString(converter.Result));
+        }
+
+        
+    }
+}
