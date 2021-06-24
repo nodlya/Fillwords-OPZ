06c54d0de7999113bfb82f3a4acabab681bb2947
nodlya <nadya-popova-02@mail.ru>
2021-06-24 11:00:34 +0500

    2-3.1 Library works

--- OPZ/OPZ.Console/OPZ.Console/Program.cs
+++ OPZ/OPZ.Console/OPZ.Console/Program.cs
@@ -1,11 +1,18 @@
 ﻿namespace OPZConsole
 {
     using System;
+    using System.Globalization;
+
     class Program
     {
         static void Main(string[] args)
         {
-            Drawer drawer = new Drawer();   
+            string function = Drawer.AskFunction("Введите функцию: у= ");
+            double step = Drawer.AskValue(1, "Введите шаг: ");
+            double start = Drawer.AskValue(2, "Введите начало: ");
+            double end = Drawer.AskValue(3, "Введите конец: ");
+
+            Drawer.GiveTable(function, step, start, end);
         }
     }
 }
